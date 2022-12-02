27a55e3934e67a2f618d797e1891466d9a400126
Tung Phan <tungphan@alfatech.jp>
2022-12-02 17:49:11 +0900

    Iterator パターン

---
+++ Iterator/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+namespace Iterator
+{
+    public interface Aggregate
+    {
+        public abstract Iterator iterator();
+    }
+
+    public interface Iterator
+    {
+        //次の要素を持つかどうかを確認するために使用する。
+        public abstract bool hasNext();
+        //次の要素があった際に次の要素を取得するために使用する。
+        public abstract object next();
+    }
+
+
+    public class Employee
+    {
+        string name;
+        int numberCode;
+
+        public Employee(string name, int numberCode)
+        {
+            this.name = name;
+            this.numberCode = numberCode;
+        }
+
+        public string GetName() => name;
+        public int GetNumberCode() => numberCode;
+    }
+
+    /// <summary> ConcreateAggregate </summary>
+
+    public class EmployeeList : Aggregate
+    {
+        protected List<Employee> Employees;
+        public EmployeeList() 
+        {
+            this.Employees = new List<Employee>();
+        }
+
+        public void Add(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+
+        public int GetLength() => Employees.Count;
+
+        public Employee GetAt(int index) => Employees[index];
+
+        public Iterator iterator() => new EmployeeListIterator(this);
+
+    }
+
+    /// <summary> ConcreateIterator </summary>
+    public class EmployeeListIterator : Iterator
+    {
+        EmployeeList Employees;
+        int index;
+        public EmployeeListIterator(EmployeeList employees)
+        {
+            this.Employees = employees;
+            this.index = 0;
+        }
+
+        public bool hasNext()
+        {
+            return index < Employees.GetLength();
+        }
+
+        public object next()
+        {
+            var employee = Employees.GetAt(index);
+            index++;
+            return employee;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new EmployeeList();
+            employees.Add(new Employee("愛子", 1));
+            employees.Add(new Employee("順子", 2));
+            employees.Add(new Employee("美月", 3));
+            employees.Add(new Employee("夏見", 4));
+            var it = employees.iterator();
+            while (it.hasNext())
+            {
+                var em = (Employee)it.next();
+                Console.WriteLine(em.GetName() + "/" + em.GetNumberCode());
+            }
+        }
+    }
+}
---
+++ Iterator/README.md
@@ -0,0 +1,62 @@
+# Iterator パターン 
+集約オブジェクトを内包するオブジェクトが内部表現を公開することなく, その要素に順にアクセスする方法を提供する
+## Iteratorパターンのクラス図
+```mermaid
+
+classDiagram
+class Aggregate {
+	<<interface>>
+	iterator()
+}
+
+class ConcreteAggregate {
+	iterator()
+}
+
+class Iterator {
+	<<interface>>
+	+hasNext()
+	+next()
+}
+
+class ConcreteIterator {
+  +aggreate
+	+hasNext()
+	+next()
+}
+        
+Aggregate <|-- ConcreteAggregate
+Aggregate --> Iterator : create
+Iterator <|--  ConcreteIterator
+ConcreteIterator --> ConcreteAggregate
+ConcreteIterator o-- ConcreteAggregate
+
+```
+### Iterator の　役割り
+1. Iterator (反復子):
+要素に順次アクセスするインタフェースを定める.
+2. ConcreteIterator (具体的な反復子):
+[Iterator] が定めたインタフェースを実装する. 順次アクセス方法, 次要素の有無などの手順の詳細処理を定義する. また, 上記処理の対象となるオブジェクト (ConcreteAggregate) を生成時 (コンストラクタ処理) に保持しておく.
+3. Aggregate (集合体):
+[Iterator] を作り出すインタフェースを定める.
+4. ConcreteAggregate (具体的な集合体):
+[Aggregate] が定めたインタフェースを実装する. [Iterator] メソッドで, 自身のオブジェクトをコンストラクタ引数に [ConcreteAggregate] のオブジェクトを返す.
+5. Item (集合体の各要素):
+集合体の要素.
+Client (利用者):
+[Iterator] パターンを適用したクラスを利用し処理する.
+## 具体例
+具体例として、「従業員をまとめる従業員リスト」と「従業員リストのIterator」をもとに説明します。
+### 実装クラス
+#### Employeeクラス
+Employee(従業員)クラスは集合体の要素を表します。
+名前と従業員コードを持ちます。
+#### EmployeeListクラス
+EmployeeList(従業員リスト)クラスは従業員の集合体を表します。
+従業員の配列と添え字を持ちます。
+iterator()において自身を引数としてEmployeeListIteratorを生成して返しています。
+#### EmployeeListIteratorクラス
+EmployeeListIterator(従業員リストIterator)クラスは従業員の集合体を数え上げるものを表します。
+従業員リストと添え字を持ちます。
+hasNext()で従業員リストが次の要素を持つかを確認し、next()で従業員リストの次の要素(従業員)を返却しています。 
+
