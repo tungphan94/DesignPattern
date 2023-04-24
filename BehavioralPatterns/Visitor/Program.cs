d539041a2f722ceb812b718756e461b343638663
Tung Phan <tungphan@alfatech.jp>
2023-04-24 16:11:05 +0900

    　visitor パータンを作成

---
+++ BehavioralPatterns/Visitor/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace visitorExp
+{
+    /// <summary>Element クラス </summary>
+    public abstract class Shape
+    {
+        public abstract void accept(Visitor visitor);
+    }
+    /// <summary>concreteElementA クラス</summary>
+    public class CircleShape : Shape
+    {
+        private double radius;
+        public CircleShape(double radius)
+        {
+            this.radius = radius;
+        }
+        public override void accept(Visitor visitor)
+        {
+            visitor.visit(this);
+        }
+
+        public double getPerimeter() => 2 * radius * Math.PI;
+        public double getArea() => Math.PI * radius * radius;
+   
+    }
+
+    /// <summary>concreteElementB クラス</summary>
+    public class RectangleShape : Shape
+    {
+        private double w;
+        private double h;
+        public RectangleShape(double w, double h)
+        {
+            this.w = w;
+            this.h = h;
+        }
+        public override void accept(Visitor visitor)
+        {
+            visitor.visit(this);
+        }
+
+        public double getPerimeter() => 2 * (w + h);
+        public double getArea() => w * h;
+    }
+
+    /// <summary>Visitor クラス</summary>
+
+    public abstract class Visitor
+    {
+        public abstract void visit(CircleShape circle);
+        public abstract void visit(RectangleShape B);
+    }
+
+    /// <summary>concreteVisitor1 クラス </summary>
+    public class PerimeterVisitor : Visitor
+    {
+        public override void visit(CircleShape A)
+        {
+            Console.WriteLine($"円の周囲は: {A.getPerimeter()}");
+        }
+        public override void visit(RectangleShape B)
+        {
+            Console.WriteLine($"長方形の周囲は: {B.getPerimeter()}");
+        }
+    }
+
+    /// <summary>concreteVisitor2 クラス </summary>
+
+    public class AreaVisitor : Visitor
+    {
+        public override void visit(CircleShape A)
+        {
+            Console.WriteLine($"円の面積は: {A.getArea()}");
+
+        }
+        public override void visit(RectangleShape B)
+        {
+            Console.WriteLine($"長方形の面積は: {B.getArea()}");
+        }
+    }
+
+    /// <summary> </summary>
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            var perimeterVisitor = new PerimeterVisitor();
+            var circle = new CircleShape(10);
+            var rect = new RectangleShape(10,20);
+            circle.accept(perimeterVisitor);
+            rect.accept(perimeterVisitor);
+
+            var areaVisitor = new AreaVisitor();
+            circle.accept(areaVisitor);
+            rect.accept(areaVisitor);
+        }
+    }
+}
---
+++ BehavioralPatterns/Visitor/README.md
@@ -0,0 +1,57 @@
+# Visitor パターン
+[Visitor] = [訪問者]
+
+このパターンは, [データ構造] と [それに体する処理] を分離することを目的とするパターンである. そのため, このパターンを適用すると, [データ構造] を変更することなしに, [新しい処理] を追加することができる.
+
+具体的には, 訪問者である Visitor 役のオブジェクトが, 訪問先であるデータ構造要素の個々のオブジェクトを訪問し, その訪問先の公開されている資源を利用して処理を実行して回るという形になる. (データ構造役オブジェクトは, 処理を訪問者役オブジェクトに委任する)
+
+## Visitorパターンのクラス図
+```mermaid
+classDiagram
+
+
+class Visitor {
+          <<interface>>
+          +visit(ConcreteElememt1)
+          +visit(ConcreteElememt2)
+          }
+class ConcreteVisitor1 {
+          +visit(ConcreteElememt1)
+          +visit(ConcreteElememt2)
+          }
+class ConcreteVisitor2 {
+          +visit(ConcreteElememt1)
+          +visit(ConcreteElememt2)
+          }
+class Element {
+          <<interface>>
+          + accept(Visitor)
+          }
+class ConcreteElement1 {
+          + accept(Visitor)
+          }
+class ConcreteElement2 {
+          + accept(Visitor)
+          }
+class Client {
+          }          
+Visitor  <|-- ConcreteVisitor1
+Visitor  <|-- ConcreteVisitor2
+Element  <|.. ConcreteElement1
+Element  <|.. ConcreteElement2
+```
+### Visitor の役割り
+1. Visitor (訪問者):
+具体的なデータ構造の要素 (ConcreteElement[A|B]) 毎に訪問して行う処理 (visit メソッド) のインタフェースを定義する. visit メソッドは, オーバーロードすることで, その訪問先要素の型に応じた処理を指定する.
+2. ConcreteVisitor[1|2] (具体的訪問者):
+[Visitor] で定義したインタフェース (各 visit メソッド) を実装する.
+3. Element (データ構造 (受入者)):
+[Visitor] の訪問先であるデータ構造要素に, 受入口 (accept メソッド) のインタフェースを実装する.
+4. ConcretElement[1|2] (具体的データ構造 (受入者)):
+[Element] で定義したインタフェース (accept メソッド) を実装する.
+5. Client (利用者):
+[Visitor] パターンを適用したクラスを用い処理を行う.
+
+# サンプルコードの解説
+コマンド パターンの説明のため、文字列表示の機能を実装します。
+
